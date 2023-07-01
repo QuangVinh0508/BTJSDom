39c1d6bd91087882e029ef777bdc6c3aa372c395
Quang Vinh <tranquangvinh2017tv@gmail.com>
2023-07-01 21:21:56 +0700

    Done 23.

---
+++ assets/JS/jsDom23.js
@@ -0,0 +1,33 @@
+const zoom = document.querySelector(".zoom");
+const wrapImg = document.querySelectorAll(".zoom .zoom__image");
+const result = document.querySelector(".zoom .result");
+
+const size = 4;
+
+wrapImg.forEach((item) => {
+  item.addEventListener("mousemove", function (e) {
+    result.classList.remove("hide");
+
+    const img = item.querySelector("img");
+
+    let x = (e.offsetX / this.offsetWidth) * 100;
+    let y = (e.offsetY / this.offsetHeight) * 100;
+
+    // move result
+    let posX = e.pageX - zoom.offsetLeft;
+    let posY = e.pageY - zoom.offsetTop;
+
+    result.style.cssText = `
+			background-image: url(${img.src});
+			background-size: ${img.width * size}px ${img.height * size}px;
+			background-position : ${x}% ${y}%;
+			left: ${posX}px;
+			top: ${posY}px;
+		`;
+  });
+
+  item.addEventListener("mouseleave", function (e) {
+    result.style = ``;
+    result.classList.add("hide");
+  });
+});
